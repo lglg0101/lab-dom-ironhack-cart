62041cd9ea53782d49f6114d1186b4b8a032730f
Lissy Gundersen <user@Users-MacBook-Pro.local>
2019-10-29 18:43:36 +0000

    Completed, not bonuses

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -1,28 +1,31 @@
-// ITERATION 1
 
 function updateSubtotal($product) {
-  // ...
+
+  //make a new variable that selects the number of the price 
+  const $price = Number($product.querySelector('.price span').innerText);
+  //make a new variable that selects the input value
+  const $quantity = $product.querySelector('.quantity input').valueAsNumber;
+  //make a new variable that does the math
+  const subtotalValue = $price * $quantity;
+  //make a new variable that selects the subtotal span 
+  const $subTotal = $product.querySelector('.subtotal span');
+//changes the subtotal inner text for the new value
+  $subTotal.innerText = subtotalValue;
+//returns subtotal value
+  return subtotalValue;
 }
 
 function calculateAll() {
-  // ITERATION 2
-  //...
-  // ITERATION 3
-  //...
+  const $products = document.getElementsByClassName('product');
+  let totalValue = 0;
+
+  for (let $product of $products) {
+    totalValue += updateSubtotal($product);
+  }
+
+  document.querySelector('#total-value span').innerHTML = totalValue;
 }
+//button event 
 
 const $calculateTrigger = document.getElementById('calculate');
-
 $calculateTrigger.addEventListener('click', calculateAll);
-
-// ITERATION 4
-
-function addProductRemoveListener($removeButton) {
-  // ...
-}
-
-// ITERATION 5
-
-function createProduct(event) {
-  // ...
-}
